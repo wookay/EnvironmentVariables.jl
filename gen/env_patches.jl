a4eb258455f1c89e86ff5e661d3feaf709a631fe
WooKyoung Noh <wookay.noh@gmail.com>
2026-07-31 21:38:59 +0900

    improve `Patch`

--- gen/env_patches.jl
+++ gen/env_patches.jl
@@ -3,60 +3,89 @@ using PathStrings
 
 struct Patch
     version::VersionNumber
-    envvar::EnvVarString
     filepath::PathString
-    signature::String
+    vars::Vector{Pair{EnvVarString, String}}
+    function Patch(version::VersionNumber, filepath::PathString, env_vars::Pair{EnvVarString, String}...)
+        vars::Vector{Pair{EnvVarString, String}} = collect(env_vars)
+        new(version, filepath, vars)
+    end
 end
 
 const ENV_PATCHES = Vector{Patch}([
     # v1.14
+    Patch(v"1.14.0-DEV.2614", # julia commit 6aa38364cb    Add the objcache (LMDB-based cache for LLVM compilation)
+        Path"src/objcache.cpp",
+        EnvVar"JULIA_OBJCACHE_CAPACITY" => "static const size_t OBJCACHE_CAPACITY",
+        EnvVar"JULIA_OBJCACHE" => "void ObjCache::initDB()",
+        EnvVar"JULIA_OBJCACHE_LOG" => "static FILE *getLogFile()",
+        EnvVar"JULIA_OBJCACHE_PATH" => "static std::optional<std::string> getCachePath() JL_CANSAFEPOINT"
+    ),
+    Patch(v"1.14.0-DEV.2582", # julia commit a6cf20fa5d    Integrate MMTk binding into Julia build (src/gc-mmtk)
+        Path"src/gc-mmtk/gc-mmtk.c",
+        EnvVar"MMTK_MIN_HSIZE" => "void jl_gc_init(void)",
+        EnvVar"MMTK_MIN_HSIZE_G" => "void jl_gc_init(void)",
+        EnvVar"MMTK_MAX_HSIZE" => "void jl_gc_init(void)",
+        EnvVar"MMTK_MAX_HSIZE_G" => "void jl_gc_init(void)",
+    ),
     Patch(v"1.14.0-DEV.2425", # julia commit e319a303a7    precompilation: coordinate number of workers and imaging threads via a jobserver
-        EnvVar"JULIA_PRECOMPILE_JOBSERVER",
         Path"src/aotcompile.cpp",
-        "struct JobserverClient { bool open() }",
+        EnvVar"JULIA_PRECOMPILE_JOBSERVER" => "struct JobserverClient { bool open() }"
     ),
     Patch(v"1.14.0-DEV.2313", # julia commit acaea7226f    Add dynamic ASAN detection for RTLD_DEEPBIND / `dlopen` workarounds
-        EnvVar"JULIA_ASAN_COMPAT",
         Path"src/dlload.c",
-        "int jl_running_under_sanitizer(int recheck) JL_NOTSAFEPOINT",
+        EnvVar"JULIA_ASAN_COMPAT" => "int jl_running_under_sanitizer(int recheck) JL_NOTSAFEPOINT",
+        EnvVar"JULIA_USE_RTLD_DEEPBIND" => "static int jl_use_rtld_deepbind(int recheck) JL_NOTSAFEPOINT"
     ),
-    Patch(v"1.14.0-DEV.2313", # julia commit acaea7226f    Add dynamic ASAN detection for RTLD_DEEPBIND / `dlopen` workarounds
-        EnvVar"JULIA_USE_RTLD_DEEPBIND",
-        Path"src/dlload.c",
-        "static int jl_use_rtld_deepbind(int recheck) JL_NOTSAFEPOINT",
+
+    # v1.12
+    Patch(v"1.12.0-DEV.1578", # julia commit f336314762    Make heap size hint available as an env variable
+        Path"src/gc-stock.c",
+        EnvVar"JULIA_HEAP_SIZE_HINT" => "void jl_gc_init(void)"
     ),
 
     # v1.10
+    Patch(v"1.10.0-DEV.964",  # julia commit 8e0cba5663    timing: add envvars for subsystem enable and verbose metadata
+        Path"src/jlapi.c",
+        EnvVar"JULIA_WAIT_FOR_TRACY" => "JL_DLLEXPORT int jl_repl_entrypoint(int argc, char *argv[]) JL_CANSAFEPOINT_ENTER_LEAVE"
+    ),
     Patch(v"1.10.0-DEV.716",  # julia commit 4e35f416bb    Clean up timers and prints, link to JULIA_IMAGE_TIMINGS
-        EnvVar"JULIA_IMAGE_TIMINGS",
         Path"src/aotcompile.cpp",
-        """template<typename ModuleReleasedFunc>
+        EnvVar"JULIA_IMAGE_TIMINGS" => """
+template<typename ModuleReleasedFunc>
 static SmallVector<AOTOutputs, 16> add_output(Module &M, TargetMachine &TM, StringRef name, unsigned threads,
                 bool unopt_out, bool opt_out, bool obj_out, bool asm_out,
                 JobserverClient *jobserver, ModuleReleasedFunc module_released)"""
     ),
     Patch(v"1.10.0-DEV.715",  # julia commit 8cf48f2369    Don't inject CRT aliases on macos
-        EnvVar"JULIA_CPU_THREADS",
         Path"src/aotcompile.cpp",
-        "static unsigned compute_image_thread_count(const ModuleInfo &info, bool jobserver_active)"
+        EnvVar"JULIA_CPU_THREADS" => "static unsigned compute_image_thread_count(const ModuleInfo &info, bool jobserver_active)"
     ),
     Patch(v"1.10.0-DEV.703",  # julia commit 3915101dc6    Multithreaded image builder
-        EnvVar"JULIA_IMAGE_THREADS",
         Path"src/aotcompile.cpp",
-        "static unsigned compute_image_thread_count(const ModuleInfo &info, bool jobserver_active)"
+        EnvVar"JULIA_IMAGE_THREADS" => "static unsigned compute_image_thread_count(const ModuleInfo &info, bool jobserver_active)"
     ),
 
     # v1.8
     Patch(v"1.8.0-DEV.661",   # julia commit 628209c1f2    separate codegen/LLVM from julia runtime
-        EnvVar"ENABLE_GDBLISTENER",
         Path"src/codegen.cpp",
-        "extern "C" void jl_init_llvm(void)"
+        EnvVar"ENABLE_GDBLISTENER" => """extern "C" void jl_init_llvm(void)"""
+    ),
+
+    # v0.7
+    Patch(v"0.7.0-DEV.2999",  # julia commit 6885af8c94    rename JULIA_HOME/JULIA_HOME => JULIA_BINDIR/Sys.BINDIR
+        Path"src/jlapi.c",
+        EnvVar"JULIA_BINDIR" => "static void jl_resolve_sysimg_location(JL_IMAGE_SEARCH rel, const char* julia_bindir)"
+    ),
+
+    # v0.4
+    Patch(v"0.4.0-dev+2494",  # julia commit b60e7306d3    GC debugging tweaks
+        Path"src/gc-debug.c",
+        EnvVar"JULIA_GC_WAIT_FOR_DEBUGGER" => "void jl_gc_debug_init(void)"
     ),
 
     # v0.2
     Patch(v"0.2.0-rc1+50",    # julia commit a13bce29ae    Enble LLVM support for Intel VTune Amplifier if platform supports it
-        EnvVar"ENABLE_JITPROFILING",
         Path"src/codegen.cpp",
-        "extern "C" void jl_init_llvm(void)"
+        EnvVar"ENABLE_JITPROFILING" => """extern "C" void jl_init_llvm(void)"""
     ),
 ])
