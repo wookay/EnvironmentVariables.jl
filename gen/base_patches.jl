8b8bc00e104f7fef584da8a766df46b1e8eafd17
WooKyoung Noh <wookay.noh@gmail.com>
2026-08-02 16:33:23 +0900

    complete base_patches

--- gen/base_patches.jl
+++ gen/base_patches.jl
@@ -4,9 +4,21 @@ using PathStrings
 
 # ENV
 # get(ENV
+# haskey(ENV
 # Base.get_bool_env(
 
-signature_base_precompilation_do_precompile = """
+const signature_base_loading_compilecache_path = """
+function compilecache_path(pkg::PkgId, prefs_blob::String; flags::CacheFlags=CacheFlags(), project::String=something(Base.active_project(), ""))::String"""
+const signature_base_loading_create_expr_cache = """
+function create_expr_cache(pkg::PkgId, input::PkgLoadSpec, output::String, output_o::Union{Nothing, String},
+                           concrete_deps::typeof(_concrete_dependencies), flags::Cmd=``, cacheflags::CacheFlags=CacheFlags(),
+                           internal_stderr::IO = stderr, internal_stdout::IO = stdout, loadable_exts::Union{Vector{PkgId},Nothing}=nothing;
+                           report_timing::Bool=false)"""
+const signature_base_loading_include_package_for_output = """
+function include_package_for_output(pkg::PkgId, input::String, syntax_version::VersionNumber, depot_path::Vector{String}, dl_load_path::Vector{String}, load_path::Vector{String},
+                                    concrete_deps::typeof(_concrete_dependencies), source::Union{Nothing,String})"""
+
+const signature_base_precompilation_do_precompile = """
 function do_precompile(pkgs::Union{Vector{String}, Vector{PkgId}},
                        internal_call::Bool,
                        strict::Bool,
@@ -19,24 +31,20 @@ function do_precompile(pkgs::Union{Vector{String}, Vector{PkgId}},
                        manifest::Bool,
                        ignore_loaded::Bool,
                        detachable::Bool,
-                       work_channel::Channel{PrecompileRequest})
-"""
-signature_base_precompilation_monitor_background_precompile = """
+                       work_channel::Channel{PrecompileRequest})"""
+const signature_base_precompilation_monitor_background_precompile = """
 function monitor_background_precompile(io::IO = stderr, detachable::Bool = true, wait_for_pkg::Union{Nothing, PkgId} = nothing;
                                        key_controls::Union{Bool, Nothing} = nothing)"""
-signature_base_loading_create_expr_cache = """
-function create_expr_cache(pkg::PkgId, input::PkgLoadSpec, output::String, output_o::Union{Nothing, String},
-                           concrete_deps::typeof(_concrete_dependencies), flags::Cmd=``, cacheflags::CacheFlags=CacheFlags(),
-                           internal_stderr::IO = stderr, internal_stdout::IO = stdout, loadable_exts::Union{Vector{PkgId},Nothing}=nothing;
-                           report_timing::Bool=false)
-"""
-signature_base_loading_include_package_for_output = """
-function include_package_for_output(pkg::PkgId, input::String, syntax_version::VersionNumber, depot_path::Vector{String}, dl_load_path::Vector{String},
-                                    load_path::Vector{String}, concrete_deps::typeof(_concrete_dependencies), source::Union{Nothing,String})
-"""
-signature_base_loading_compilecache_path = """
-function compilecache_path(pkg::PkgId, prefs_blob::String; flags::CacheFlags=CacheFlags(), project::String=something(Base.active_project(), ""))::String
-"""
+
+const signature_base_terminfo_ttyhastruecolor = "function ttyhastruecolor()"
+
+const signature_base_util_runtests = """
+function runtests(tests = ["all"]; ncores::Int = ceil(Int, Sys.EFFECTIVE_CPU_THREADS / 2),
+                  exit_on_error::Bool=false,
+                  revise::Bool=false,
+                  propagate_project::Bool=false,
+                  seed::Union{BitInteger,Nothing}=nothing,
+                  julia_args::Cmd=``)"""
 
 const BASE_PATCHES = Vector{Patch}([
     # v1.14
@@ -50,6 +58,10 @@ const BASE_PATCHES = Vector{Patch}([
         EnvKey"JULIA_PRECOMPILE_JOBSERVER" => "function teardown_precompile_jobserver!()",
         EnvKey"JULIA_PRECOMPILE_THREADS" => signature_base_precompilation_do_precompile,
         EnvKey"JULIA_IMAGE_THREADS" => signature_base_precompilation_do_precompile
+    ),
+    Patch(v"1.14.0-DEV.2243", # julia commit f022e008b5    precompilepkgs: add more timing info in timing & verbose mode
+        Path"base/loading.jl",
+        EnvKey"JULIA_PRECOMP_REPORT_TIMING" => signature_base_loading_include_package_for_output
     ),
     Patch(v"1.14.0-DEV.2014", # julia commit 9b03f4ae1b    Redact sensitive env vars when printing cmd errors
         Path"base/cmd.jl",
@@ -68,6 +80,10 @@ const BASE_PATCHES = Vector{Patch}([
         EnvKey"CI_DEBUG_TRACE" => "function exec_options(opts)",
         EnvKey"SYSTEM_DEBUG" => "function exec_options(opts)"
     ),
+    Patch(v"1.13.0-DEV.840",  # julia commit 72e2c45554    Make current_terminfo a OncePerProcess
+        Path"base/terminfo.jl",
+        EnvKey"TERM" => "const current_terminfo"
+    ),
 
     # v1.12
     Patch(v"1.12.0-DEV.1244", # julia commit a06a80162b    Add filesystem func to transform a path to a URI
@@ -83,6 +99,10 @@ const BASE_PATCHES = Vector{Patch}([
         EnvKey"FORCE_COLOR" => "function colored_text(opts::JLOptions)",
         EnvKey"NO_COLOR" => "function colored_text(opts::JLOptions)"
     ),
+    Patch(v"1.12.0-DEV.151",  # julia commit 78351b5c47    Use Base parallel precompilation to build stdlibs
+        Path"base/util.jl",
+        EnvKey"JULIA_TESTS" => signature_base_util_runtests
+    ),
     Patch(v"1.12.0-DEV.128",  # julia commit 67451604cf    Move parallel precompilation to Base
         Path"base/precompilation.jl",
         EnvKey"CI" => "can_fancyprint(io::IO)",
@@ -90,9 +110,25 @@ const BASE_PATCHES = Vector{Patch}([
     ),
 
     # v1.11
+    Patch(v"1.11.0-DEV.897",  # julia commit 9729f31218    Tweak test LOAD_PATH and fix loading test
+        Path"base/util.jl",
+        EnvKey"JULIA_LOAD_PATH" => signature_base_util_runtests
+    ),
     Patch(v"1.11.0-DEV.504",  # julia commit e7290dc1fc    Add environment variable to force the use of the fallback repl
         Path"base/client.jl",
         EnvKey"JULIA_FALLBACK_REPL" => "function run_main_repl(interactive::Bool, quiet::Bool, banner::Symbol, history_file::Bool)"
+    ),
+    Patch(v"1.11.0-DEV.307",  # julia commit 2690ca8cc6    Terminfo parser
+        Path"base/terminfo.jl",
+        EnvKey"TERMINFO" => "function find_terminfo_file(term::String)",
+        EnvKey"TERMINFO_DIRS" => "function find_terminfo_file(term::String)",
+        EnvKey"COLORTERM" => signature_base_terminfo_ttyhastruecolor,
+        EnvKey"VTE_VERSION" => signature_base_terminfo_ttyhastruecolor,
+        EnvKey"XTERM_VERSION" => signature_base_terminfo_ttyhastruecolor,
+        EnvKey"TERMINAL_PROGRAM" => signature_base_terminfo_ttyhastruecolor,
+        EnvKey"KONSOLE_PROFILE_NAME" => signature_base_terminfo_ttyhastruecolor,
+        EnvKey"KONSOLE_DBUS_SESSION" => signature_base_terminfo_ttyhastruecolor,
+        EnvKey"TERM" => signature_base_terminfo_ttyhastruecolor
     ),
 
     # v1.10
@@ -105,17 +141,49 @@ const BASE_PATCHES = Vector{Patch}([
         EnvKey"JULIA_CPU_TARGET" => signature_base_loading_create_expr_cache,
         EnvKey"JULIA_CPU_TARGET" => signature_base_loading_compilecache_path
     ),
+    Patch(v"1.10.0-DEV.204",  # julia commit a2db90fe8d    Implement support for object caching through pkgimages
+        Path"base/linking.jl",
+        EnvKey"JULIA_VERBOSE_LINKING" => "verbose_linking()"
+    ),
+
+    # v1.8
+    Patch(v"1.8.0-DEV.1185",  # julia commit f61eb87cd8    `Base.runtests`: unset `JULIA_PROJECT` and `JULIA_LOAD_PATH`
+        Path"base/util.jl",
+        EnvKey"JULIA_PROJECT" => signature_base_util_runtests
+    ),
+    Patch(v"1.8.0-DEV.590",   # julia commit 2c9e051c46    `Base.runtests`: set a temporary Pkg depot path
+        Path"base/util.jl",
+        EnvKey"JULIA_DEPOT_PATH" => signature_base_util_runtests
+    ),
 
     # v1.6
     Patch(v"1.6.0-DEV.1167",  # julia commit 58605d3702    Make it possible to override MAX_NUM_PRECOMPILE_FILES using ENV
         Path"base/Base.jl",
         EnvKey"JULIA_MAX_NUM_PRECOMPILE_FILES" => "function __init__()"
     ),
+    Patch(v"1.6.0-DEV.354",   # julia commit 30b09dfd2b    [WIP] include ClearStacktrace.jl in Base
+        Path"base/errorshow.jl",
+        EnvKey"JULIA_STACKTRACE_EXPAND_BASEPATHS" => "stacktrace_expand_basepaths()::Bool",
+        EnvKey"JULIA_STACKTRACE_CONTRACT_HOMEDIR" => "stacktrace_contract_userdir()::Bool",
+        EnvKey"JULIA_STACKTRACE_LINEBREAKS" => "stacktrace_linebreaks()::Bool"
+    ),
 
     # v0.7
+    Patch(v"0.7.0-beta.280",  # julia commit 9050651b24    Rename Sys.CPU_CORES => Sys.CPU_THREADS
+        Path"base/sysinfo.jl",
+        EnvKey"JULIA_CPU_THREADS" => "module Sys function __init__()"
+    ),
+    Patch(v"0.7.0-beta.280",  # julia commit 9050651b24    Rename Sys.CPU_CORES => Sys.CPU_THREADS
+        Path"base/util.jl",
+        EnvKey"JULIA_CPU_THREADS" => signature_base_util_runtests
+    ),
     Patch(v"0.7.0-beta.90",   # julia commit 2549e227cf    Pkg + code loading: home project & active project concepts
         Path"base/initdefs.jl",
         EnvKey"JULIA_PROJECT" => "function init_active_project()"
+    ),
+    Patch(v"0.7.0-DEV.5168",  # julia commit a348496165    Add `Sys.which(program_name::AbstractString)`
+        Path"base/sysinfo.jl",
+        EnvKey"PATH" => "module Sys function which(program_name::String)"
     ),
     Patch(v"0.7.0-DEV.4898",  # julia commit 50413a46a0    change default LOAD_PATH and DEPOT_PATH
         Path"base/loading.jl",
@@ -128,6 +196,11 @@ const BASE_PATCHES = Vector{Patch}([
     ),
 
     # v0.4
+    Patch(v"0.4.0-dev+1914",  # julia commit 52fecaf2c6    deprecate tty_size, replace with iosize & export it
+        Path"base/stream.jl",
+        EnvKey"LINES" => "displaysize()",
+        EnvKey"COLUMNS" => "displaysize()"
+    ),
     Patch(v"0.4.0-dev+1852",  # julia commit e84d85dcac    Fix #9103, implementing `cd -` in REPL shell mode
         Path"base/client.jl",
         EnvKey"OLDPWD" => "function repl_cmd(cmd::AbstractCmd, out)"
@@ -135,6 +208,10 @@ const BASE_PATCHES = Vector{Patch}([
     Patch(v"0.4.0-dev+1734",  # julia commit 571a8c779e    split client.jl into REPL-related pieces and basic initialization definitions
         Path"base/initdefs.jl",
         EnvKey"JULIA_LOAD_PATH" => "function init_load_path()"
+    ),
+    Patch(v"0.4.0-dev+1604",  # julia commit 6c5265316b    `@threadcall`: ccall-like macro for doing work in another thread
+        Path"base/threadcall.jl",
+        EnvKey"UV_THREADPOOL_SIZE" => "const max_ccall_threads"
     ),
 
     # v0.2
