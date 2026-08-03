43447f5c4b5b068542547e00791bd899da4a4163
WooKyoung Noh <wookay.noh@gmail.com>
2026-08-03 22:35:44 +0900

    add docs/src/Docs.md

--- docs/make.jl
+++ docs/make.jl
@@ -16,6 +16,7 @@ makedocs(
     authors = "WooKyoung Noh",
     pages = Any[
         "Home" => "index.md",
+        "EnvironmentVariables.Docs" => "Docs.md",
         "src/ cli/ ENV variables" => "src_patches.md",
         "base/ ENV variables" => "base_patches.md",
         "STDLIB ENV variables" => "stdlib_patches.md",
